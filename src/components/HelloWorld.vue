3189b854dc5465e4f71b3ccbc9e9b19452306a52
Dmitriy Bondarenko <dmitriy.bondarenko@abz.agency>
2021-06-27 11:26:13 +0300

    test input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
+      <test-input/>
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
@@ -31,8 +32,11 @@
 </template>
 
 <script>
+import testInput from '../components/test-input.vue'
+
 export default {
   name: 'HelloWorld',
+  components: { testInput},
   props: {
     msg: String,
   },
